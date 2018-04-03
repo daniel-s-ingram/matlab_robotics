83d414e46210ba4d999d883a966f01b0e4b96d15
Daniel Ingram <ingramds@appstate.edu>
2018-04-02 22:26:21 -0400

    Break out of loop with Escape key

--- Ch4/follow_line.m
+++ Ch4/follow_line.m
@@ -7,7 +7,7 @@ if nargin == 0
     Kh = 0.1;
 end
 
-vehicle = SimpleVehicle(0, 0, 0, 1);
+vehicle = SimpleVehicle(0, 0, pi/4, 1);
 
 dt = 0.01;
 
@@ -17,13 +17,18 @@ y = vehicle.y;
 theta = vehicle.theta;
 theta_des = atan2(-a, b);
 
-figure(1);
+global running
+running = true;
+
+fig = gcf;
+set(fig, 'KeyPressFcn', @KeyPressed);
+
 xl = -50:50;
 yl = (-a/b)*xl + (-c/b);
 plot(xl, yl);
 hold on;
 
-while true
+while running
     theta = theta - Kd*(dot([a, b, c], [x, y, 1])/sqrt(a^2+b^2)) + Kh*angdiff(theta_des, theta);
     x = x + v*cos(theta)*dt;
     y = y + v*sin(theta)*dt;
@@ -35,12 +40,20 @@ while true
     pause(dt);
 end
 
+close;
+
 end
 
 function PlotPose(vehicle)
-    figure(1);
     plot([vehicle.front(1), vehicle.back_right(1)], [vehicle.front(2), vehicle.back_right(2)], 'r-', ...
          [vehicle.back_right(1), vehicle.back_left(1)], [vehicle.back_right(2), vehicle.back_left(2)], 'b-', ...
          [vehicle.back_left(1), vehicle.front(1)], [vehicle.back_left(2), vehicle.front(2)], 'r-');
+end
+
+function KeyPressed(~, event)
+global running
+if strcmp(event.Key, 'escape')
+    running = false;
+end
 end
 
